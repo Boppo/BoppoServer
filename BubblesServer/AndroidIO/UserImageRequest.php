888ea056c8a6eaecbdbee10a5c7a6002e499a45e
Ziomster44 <ziomster44@gmail.com>
2016-12-15 20:09:06 -0500

    Added the setImage and dbSetImage functions to the UserImageRequest and UserImage files, respectively, enabling updating most of the properties of an Image.

--- BubblesServer/AndroidIO/UserImageRequest.php
+++ BubblesServer/AndroidIO/UserImageRequest.php
@@ -3,20 +3,22 @@
 $function = $_GET['function'];
 
 if ($function == "addImagesToEvent")
-	addImagesToEvent();
+  addImagesToEvent();
 if ($function == 'getImagesByEid')
-	getImagesByEid();
+  getImagesByEid();
 if ($function == 'getImagesByUidAndPurpose')
-	getImagesByUidAndPurpose();
+  getImagesByUidAndPurpose();
 if ($function == 'getImagesByPrivacyAndPurpose')
-	getImagesByPrivacyAndPurpose();
+  getImagesByPrivacyAndPurpose();
 if ($function == 'getImageProfileMaxAmount')
-	getImageProfileMaxAmount();
+  getImageProfileMaxAmount();
+if ($function == 'setImage')
+  setImage();
 if ($function == 'uploadImage')
-	uploadImage();
+  uploadImage();
 
-	
-	
+  
+  
 /* FUNCTION: addImagesToEvent
  * DESCRIPTION: Adds the images with the uiids in the specified list to the
  *              specified event.
@@ -25,29 +27,29 @@ if ($function == 'uploadImage')
  * -------------------------------------------------------------------------------- */
 function addImagesToEvent()
 {
-	/* THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. */
-	error_reporting(E_ALL);
-	ini_set('display_errors', TRUE);
-	ini_set('display_startup_errors', TRUE);
-	/* END. */
+  /* THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. */
+  error_reporting(E_ALL);
+  ini_set('display_errors', TRUE);
+  ini_set('display_startup_errors', TRUE);
+  /* END. */
 
-	// DECODE JSON STRING //
-	$json_decoded = json_decode(file_get_contents("php://input"), true);
-	// ASSIGN THE JSON VALUES TO VARIABLES //
-	$eid   = $json_decoded["eid"];
-	$uiids = $json_decoded["uiids"];
+  // DECODE JSON STRING //
+  $json_decoded = json_decode(file_get_contents("php://input"), true);
+  // ASSIGN THE JSON VALUES TO VARIABLES //
+  $eid   = $json_decoded["eid"];
+  $uiids = $json_decoded["uiids"];
 
-	// FOR EVERY USER IMAGE IDENTIFIER (UIID), ADD IT IMAGE TO THE EVENT //
-	require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
+  // FOR EVERY USER IMAGE IDENTIFIER (UIID), ADD IT IMAGE TO THE EVENT //
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
 
-	$responses = array();
-	foreach($uiids as $uiid)
-	{
-		$response = addImageToEvent($eid, $uiid);
-		array_push($responses, $response);
-	}
+  $responses = array();
+  foreach($uiids as $uiid)
+  {
+    $response = addImageToEvent($eid, $uiid);
+    array_push($responses, $response);
+  }
 
-	echo json_encode($responses);
+  echo json_encode($responses);
 }
 /* --------------------------------------------------------------------------------
  * ================================================================================
@@ -62,28 +64,28 @@ function addImagesToEvent()
  * -------------------------------------------------------------------------------- */
 function getImagesByEid()
 {
-	/* THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. */
-	error_reporting(E_ALL);
-	ini_set('display_errors', TRUE);
-	ini_set('display_startup_errors', TRUE);
-	/* END. */
+  /* THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. */
+  error_reporting(E_ALL);
+  ini_set('display_errors', TRUE);
+  ini_set('display_startup_errors', TRUE);
+  /* END. */
 
-	// DECODE JSON STRING
-	$json_decoded = json_decode(file_get_contents("php://input"), true);
-	// ASSIGN THE JSON VALUES TO VARIABLES
-	$eid = $json_decoded["eid"];
+  // DECODE JSON STRING
+  $json_decoded = json_decode(file_get_contents("php://input"), true);
+  // ASSIGN THE JSON VALUES TO VARIABLES
+  $eid = $json_decoded["eid"];
 
-	require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
-	$images = fetchImagesByEid($eid);
+  require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
+  $images = fetchImagesByEid($eid);
 
-	// RETURN THE EVENT ID
-	echo json_encode($images);
+  // RETURN THE EVENT ID
+  echo json_encode($images);
 }
 /* --------------------------------------------------------------------------------
  * ================================================================================
  * -------------------------------------------------------------------------------- */
 
-	
+  
 
 /* FUNCTION: getImagesByUidAndPurpose
  * DESCRIPTION: Gets the images and their data by specified Uid and Purpose.
@@ -92,24 +94,24 @@ function getImagesByEid()
  * -------------------------------------------------------------------------------- */
 function getImagesByUidAndPurpose()
 {
-	/* THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. */
-	error_reporting(E_ALL);
-	ini_set('display_errors', TRUE);
-	ini_set('display_startup_errors', TRUE);
-	/* END. */
+  /* THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. */
+  error_reporting(E_ALL);
+  ini_set('display_errors', TRUE);
+  ini_set('display_startup_errors', TRUE);
+  /* END. */
 
-	// DECODE JSON STRING
-	$json_decoded = json_decode(file_get_contents("php://input"), true);
-	// ASSIGN THE JSON VALUES TO VARIABLES
-	$uid                 = $json_decoded["uid"];
-	$image_purpose_label = $json_decoded["imagePurposeLabel"];
-	$event_indicator     = $json_decoded["eventIndicator"];
-	
-	require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
-	$images = fetchImagesByUidAndPurpose($uid, $image_purpose_label, $event_indicator);
+  // DECODE JSON STRING
+  $json_decoded = json_decode(file_get_contents("php://input"), true);
+  // ASSIGN THE JSON VALUES TO VARIABLES
+  $uid                 = $json_decoded["uid"];
+  $image_purpose_label = $json_decoded["imagePurposeLabel"];
+  $event_indicator     = $json_decoded["eventIndicator"];
+  
+  require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
+  $images = fetchImagesByUidAndPurpose($uid, $image_purpose_label, $event_indicator);
 
-	// RETURN THE EVENT ID
-	echo json_encode($images);
+  // RETURN THE EVENT ID
+  echo json_encode($images);
 }
 /* --------------------------------------------------------------------------------
  * ================================================================================
@@ -124,32 +126,32 @@ function getImagesByUidAndPurpose()
  * -------------------------------------------------------------------------------- */
 function getImagesByPrivacyAndPurpose()
 {
-	// THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. //
-	error_reporting(E_ALL);
-	ini_set('display_errors', TRUE);
-	ini_set('display_startup_errors', TRUE);
-	// END. //
+  // THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. //
+  error_reporting(E_ALL);
+  ini_set('display_errors', TRUE);
+  ini_set('display_startup_errors', TRUE);
+  // END. //
 
-	// DECODE JSON STRING
-	$json_decoded = json_decode(file_get_contents("php://input"), true);
-	// ASSIGN THE JSON VALUES TO VARIABLES
-	$image_privacy_label = $json_decoded["imagePrivacyLabel"];
-	$image_purpose_label = $json_decoded["imagePurposeLabel"];
-	$event_indicator     = $json_decoded["eventIndicator"];
-	
-	require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
-	$images = fetchImagesByPrivacyAndPurpose($image_privacy_label, $image_purpose_label, 
-		$event_indicator);
+  // DECODE JSON STRING
+  $json_decoded = json_decode(file_get_contents("php://input"), true);
+  // ASSIGN THE JSON VALUES TO VARIABLES
+  $image_privacy_label = $json_decoded["imagePrivacyLabel"];
+  $image_purpose_label = $json_decoded["imagePurposeLabel"];
+  $event_indicator     = $json_decoded["eventIndicator"];
+  
+  require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
+  $images = fetchImagesByPrivacyAndPurpose($image_privacy_label, $image_purpose_label, 
+    $event_indicator);
 
-	// RETURN THE EVENT ID
-	echo json_encode($images);
+  // RETURN THE EVENT ID
+  echo json_encode($images);
 }
 /* --------------------------------------------------------------------------------
  * ================================================================================
  * -------------------------------------------------------------------------------- */
 
-	
-	
+  
+  
 /* FUNCTION:    getImageProfileMaxAmount
  * DESCRIPTION: Gets the integer that represents the maximum amount of allowed
  *              profile images. 
@@ -158,22 +160,91 @@ function getImagesByPrivacyAndPurpose()
  * -------------------------------------------------------------------------------- */
 function getImageProfileMaxAmount()
 {
-	/* THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. */
-	error_reporting(E_ALL);
-	ini_set('display_errors', TRUE);
-	ini_set('display_startup_errors', TRUE);
-	/* END. */
+  /* THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. */
+  error_reporting(E_ALL);
+  ini_set('display_errors', TRUE);
+  ini_set('display_startup_errors', TRUE);
+  /* END. */
 
-	// FETCH THE DATA REPRESENTING THE MAXIMUM AMOUNT OF ALLOWED PROFILE IMAGES.
-	$path_gv = $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/Resources/GlobalVariables.json';
-	$file_gv = file_get_contents($path_gv);
-	$array_gv = json_decode($file_gv, true);
-	$image_profile_max_amount = $array_gv["Image"]["ImageProfileMaxAmount"];
-	
-	// RETURN THE EVENT ID
+  // FETCH THE DATA REPRESENTING THE MAXIMUM AMOUNT OF ALLOWED PROFILE IMAGES.
+  $path_gv = $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/Resources/GlobalVariables.json';
+  $file_gv = file_get_contents($path_gv);
+  $array_gv = json_decode($file_gv, true);
+  $image_profile_max_amount = $array_gv["Image"]["ImageProfileMaxAmount"];
+  
+  // RETURN THE EVENT ID
     echo $image_profile_max_amount;
 }
 
+/* --------------------------------------------------------------------------------
+ * ================================================================================
+ * -------------------------------------------------------------------------------- */
+
+
+
+/* FUNCTION: setImage
+ * DESCRIPTION: Updates the image's properties in the database and filesystem.
+ * --------------------------------------------------------------------------------
+ * ================================================================================
+ * -------------------------------------------------------------------------------- */
+
+function setImage()
+{
+  // THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. //
+  error_reporting(E_ALL);
+  ini_set('display_errors', TRUE);
+  ini_set('display_startup_errors', TRUE);
+  // END. //
+
+  // ESTABLISH DATABASE CONNECTION //
+  require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBConnect/dbConnect.php';
+
+  // DECODE INCOMING JSON CONTENTS //
+  $json_decoded = json_decode(file_get_contents("php://input"), true);
+
+  $uiid = $json_decoded["uiid"];
+  $user_image_profile_sequence = $json_decoded["userImageProfileSequence"];
+  $user_image_name = $json_decoded["userImageName"];
+  $user_image_purpose_label = $json_decoded["userImagePurposeLabel"];
+  $user_image_privacy_label = $json_decoded["userImagePrivacyLabel"];
+  $user_image_gps_latitude = $json_decoded["userImageGpsLatitude"];
+  $user_image_gps_longitude = $json_decoded["userImageGpsLongitude"];
+  
+  // MAKE SURE THAT A VALID USER IMAGE IDENTIFIER WAS PROVIDED
+  if ($uiid <= 0) {
+    echo "ERROR: Incorrect user image identifier specified.";
+    return; }
+    
+  // ENCODE THE USER IMAGE PURPOSE LABEL
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/ImagePurpose.php';
+  $user_image_purpose_code = fetchImagePurposeCode($user_image_purpose_label);
+  if (!($json_decoded["userImagePurposeLabel"] == null || $user_image_purpose_code != null)) {
+    echo "ERROR: Incorrect user image purpose label specified.";
+    return; }
+    
+  // ENCODE THE PRIVACY LABEL
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/Privacy.php';
+  $user_image_privacy_code = fetchPrivacyCode($user_image_privacy_label);
+  if (!($json_decoded["userImagePrivacyLabel"] == null || $user_image_privacy_code != null)) {
+    echo "ERROR: Incorrect user image privacy specified.";
+    return; }
+
+  // SEND THE NEW VALUES IN AN EVENT OBJECT TO THE CORRESPONDING DBIO METHOD
+  $image = array
+  (
+    "uiid" => $uiid,
+    "userImageProfileSequence" => $user_image_profile_sequence,
+    "userImageName" => $user_image_name,
+    "userImagePurposeCode" => $user_image_purpose_code,
+    "userImagePrivacyCode" => $user_image_privacy_code,
+    "userImageGpsLatitude" => $user_image_gps_latitude,
+    "userImageGpsLongitude" => $user_image_gps_longitude
+  );
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
+  $response = dbSetImage($image);
+  
+  echo $response;
+}
 /* --------------------------------------------------------------------------------
  * ================================================================================
  * -------------------------------------------------------------------------------- */
@@ -188,61 +259,61 @@ function getImageProfileMaxAmount()
 
 function uploadImage()
 {
-	// THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. //
-	error_reporting(E_ALL);
-	ini_set('display_errors', TRUE);
-	ini_set('display_startup_errors', TRUE);
-	// END. //
-	
-	// ESTABLISH DATABASE CONNECTION //
-	require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBConnect/dbConnect.php';
+  // THE FOLLOWING 3 LINES OF CODE ENABLE ERROR REPORTING. //
+  error_reporting(E_ALL);
+  ini_set('display_errors', TRUE);
+  ini_set('display_startup_errors', TRUE);
+  // END. //
+  
+  // ESTABLISH DATABASE CONNECTION //
+  require $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBConnect/dbConnect.php';
 
-	// DECODE INCOMING JSON CONTENTS //
-	$json_decoded = json_decode(file_get_contents("php://input"), true);
+  // DECODE INCOMING JSON CONTENTS //
+  $json_decoded = json_decode(file_get_contents("php://input"), true);
 
-	$uid = $json_decoded["uid"];
-	$user_image_name = $json_decoded["userImageName"];
-	$user_image_purpose_label = $json_decoded["userImagePurposeLabel"];
-	$user_image_privacy_label = $json_decoded["userImagePrivacyLabel"];
-	$user_image_gps_latitude = $json_decoded["userImageGpsLatitude"];
-	$user_image_gps_longitude = $json_decoded["userImageGpsLongitude"];
-	$user_image = $json_decoded["userImage"];
-	
-	// ENCODE THE LABELS INTO CODES AND GET THE NEXT USER IMAGE SEQUENCE NUMBER //
-	require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/Privacy.php';
-	require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/ImagePurpose.php';
-	require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
-	$user_image_purpose_code = fetchImagePurposeCode($user_image_purpose_label); 
-	$user_image_privacy_code = fetchPrivacyCode($user_image_privacy_label);
-	$user_image_sequence = fetchUserImageSequence($uid);
-	
-	// UPLOAD THE IMAGE TO THE DATABASE // 
-	$query = "INSERT INTO T_USER_IMAGE (uid, user_image_sequence, user_image_name, 
-			    user_image_purpose_code, user_image_privacy_code,
+  $uid = $json_decoded["uid"];
+  $user_image_name = $json_decoded["userImageName"];
+  $user_image_purpose_label = $json_decoded["userImagePurposeLabel"];
+  $user_image_privacy_label = $json_decoded["userImagePrivacyLabel"];
+  $user_image_gps_latitude = $json_decoded["userImageGpsLatitude"];
+  $user_image_gps_longitude = $json_decoded["userImageGpsLongitude"];
+  $user_image = $json_decoded["userImage"];
+  
+  // ENCODE THE LABELS INTO CODES AND GET THE NEXT USER IMAGE SEQUENCE NUMBER //
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/Privacy.php';
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/ImagePurpose.php';
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/BubblesServer/DBIO/UserImage.php';
+  $user_image_purpose_code = fetchImagePurposeCode($user_image_purpose_label); 
+  $user_image_privacy_code = fetchPrivacyCode($user_image_privacy_label);
+  $user_image_sequence = fetchUserImageSequence($uid);
+  
+  // UPLOAD THE IMAGE TO THE DATABASE // 
+  $query = "INSERT INTO T_USER_IMAGE (uid, user_image_sequence, user_image_name, 
+          user_image_purpose_code, user_image_privacy_code,
                 user_image_gps_latitude, user_image_gps_longitude)
               VALUES (?, ?, ?, ?, ?, ?, ?)";
-	$statement = $conn->prepare($query);
-	$statement->bind_param("iisssdd", $uid, $user_image_sequence, $user_image_name, $user_image_purpose_code, 
-		$user_image_privacy_code, $user_image_gps_latitude, $user_image_gps_longitude);
-	$statement->execute();
-	$statement->error;
-	
-	// CHECK FOR AN ERROR, RETURN IT IF ONE EXISTS //
-	$error = $statement->error;
-	if ($error != "") { echo "DB ERROR: " . $error; return; }
-	
-	// DECODE THE BINARY-ENCODED IMAGE AND CREATE FOLDER & FILE STRUCTURES FOR IT //
-	$decodedUserImage = base64_decode("$user_image");
-	if (!file_exists("/var/www/Bubbles/Uploads/" . $uid))
-		mkdir("/var/www/Bubbles/Uploads/" . $uid, 0777, true);
-	if (!file_exists("/var/www/Bubbles/Uploads/" . $uid . "/" . $user_image_sequence))
-		mkdir("/var/www/Bubbles/Uploads/" . $uid . "/" . $user_image_sequence, 0777, true);
-	file_put_contents("/var/www/Bubbles/Uploads/" . $uid . "/" .
-		$user_image_sequence . "/" . $user_image_name . ".jpg", $decodedUserImage);
-	
-	echo $user_image_sequence; 
-	
-	return;
+  $statement = $conn->prepare($query);
+  $statement->bind_param("iisssdd", $uid, $user_image_sequence, $user_image_name, $user_image_purpose_code, 
+    $user_image_privacy_code, $user_image_gps_latitude, $user_image_gps_longitude);
+  $statement->execute();
+  $statement->error;
+  
+  // CHECK FOR AN ERROR, RETURN IT IF ONE EXISTS //
+  $error = $statement->error;
+  if ($error != "") { echo "DB ERROR: " . $error; return; }
+  
+  // DECODE THE BINARY-ENCODED IMAGE AND CREATE FOLDER & FILE STRUCTURES FOR IT //
+  $decodedUserImage = base64_decode("$user_image");
+  if (!file_exists("/var/www/Bubbles/Uploads/" . $uid))
+    mkdir("/var/www/Bubbles/Uploads/" . $uid, 0777, true);
+  if (!file_exists("/var/www/Bubbles/Uploads/" . $uid . "/" . $user_image_sequence))
+    mkdir("/var/www/Bubbles/Uploads/" . $uid . "/" . $user_image_sequence, 0777, true);
+  file_put_contents("/var/www/Bubbles/Uploads/" . $uid . "/" .
+    $user_image_sequence . "/" . $user_image_name . ".jpg", $decodedUserImage);
+  
+  echo $user_image_sequence; 
+  
+  return;
 } 
 /* --------------------------------------------------------------------------------
  * ================================================================================
